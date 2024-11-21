990bb8f1da87bff1370545c232692d99dbe5cd51
ASUS <ASUS@DESKTOP-PD32228>
2024-11-21 18:56:06 +0100

    First time

--- PFA/Controllers/WeatherForecastController.cs
+++ PFA/Controllers/WeatherForecastController.cs
@@ -6,9 +6,10 @@ namespace PFA.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "mohamed", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
