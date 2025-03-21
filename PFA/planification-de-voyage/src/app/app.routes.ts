278185f4c291d37a11769582e105ed61f63c0813
Emjad1257 <azddineemjad@gmail.com>
2025-03-21 10:48:46 +0000

    h

--- PFA/planification-de-voyage/src/app/app.routes.ts
+++ PFA/planification-de-voyage/src/app/app.routes.ts
@@ -21,6 +21,7 @@ import { UserMessageComponent } from './components/user-message/user-message.com
 import { AdminMessagesComponent } from './components/admin-messages/admin-messages.component';
 import { RestaurantUserComponent } from './components/restaurant-user/restaurant-user.component';
 import { PanierUserComponent } from './components/panier.user/panier-user.component';
+
 import { TransportListComponent } from './components/transport-list/transport-list.component';
 import { TransportsComponent } from './components/transports/transports.component';
 import { TransportUserComponent } from './components/transport-user/transport-user.component';
@@ -38,7 +39,7 @@ export const routes: Routes = [
   { path: 'restaurant-form/:id', component: RestaurantFormComponent, canActivate: [AuthGuard] },
   { path: 'restaurant-list', component: RestaurantListComponent, canActivate: [AuthGuard] }, // ✅ Nouvelle route pour liste des restaurants
   { path: 'clients', component: ClientsComponent, canActivate: [AuthGuard] },
-  
+
   { path: 'gestion-voyages', component: GestionVoyagesComponent, canActivate: [AuthGuard] }, // ✅ Gestion des voyages
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'about', component: AboutComponent , canActivate: [AuthGuard]},
@@ -48,10 +49,10 @@ export const routes: Routes = [
   { path: 'restaurant-user', component: RestaurantUserComponent },
   { path: 'transport-user', component: TransportUserComponent },
   { path: 'panier-user', component: PanierUserComponent },
-  
+
   { path: 'user-messages', component: UserMessageComponent },
   { path: 'admin-messages', component: AdminMessagesComponent },
-  
+
 
   { path: '**', redirectTo: '/home' }
 ];
